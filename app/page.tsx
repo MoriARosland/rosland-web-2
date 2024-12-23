08331b336b4640eddb458597df09a5187564102d
Mori Adrian Rosland <mori@live.no>
2024-12-23 20:03:45 +0100

    feat: page navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import TypeWriterHeader from "@/components/TypeWriterHeader";
 import LinkButtonRow from "@/components/ui/LinkButtonRow";
+import NavButton from "@/components/ui/NavButton";
 import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Home() {
@@ -10,13 +11,22 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex flex-col gap-6 justify-center items-center h-screen">
-      <TypeWriterHeader text="Hi there, I'm Mori👋" />
-      <p className="text-lg text-center px-20">
-        I study Electronic System Design at NTNU with a specialization in
-        digital and embedded systems and am currently in my fourth year.
-      </p>
-      <LinkButtonRow links={links} />
+    <main className="flex flex-col h-screen">
+      <div className="flex-1 flex flex-col gap-7 justify-center items-center">
+        <TypeWriterHeader text="Hi there, I'm Mori👋" />
+        <p className="text-lg text-center px-20">
+          I study Electronic System Design at NTNU with a specialization in
+          digital and embedded systems and am currently in my fourth year.
+        </p>
+        <div className="flex flex-row gap-4">
+          <NavButton text="Projects" href="/projects" />
+          <NavButton text="About me" href="/about" />
+        </div>
+      </div>
+
+      <div className="flex justify-center mb-6">
+        <LinkButtonRow links={links} />
+      </div>
     </main>
   );
 }
