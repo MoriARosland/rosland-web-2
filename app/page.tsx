2884ab5229b43baf8e8cf0a3daa8d8767a9481b9
Mori Adrian Rosland <mori@live.no>
2024-12-23 17:50:11 +0100

    feat: short bio

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import TypeWriterHeader from "@/components/TypeWriterHeader";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen">
+    <main className="flex flex-col gap-6 justify-center items-center h-screen">
       <TypeWriterHeader text="Hi there, I'm Mori👋" />
+      <p className="text-lg text-center px-20">
+        I study Electronic System Design with a specialisation in digital and
+        embedded systems at NTNU and am currently in my 4th year.
+      </p>
     </main>
   );
 }
