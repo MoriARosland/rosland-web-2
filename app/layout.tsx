b3fd45eb5d9d771a7d48e8bebecdf088bad8711e
Mori Adrian Rosland <mori@live.no>
2024-12-23 21:01:16 +0100

    feat: dynamic favicons (#3)
    * chore: remove unused svgs
    
    * feat: light and dark favicon
    
    * feat: dynamic favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,15 @@ const titillium = Titillium_Web({
 export const metadata: Metadata = {
   title: "Rosland Web",
   description:
-    "Hello there👋 I'm Mori. I'm a software and hardwareengineer. Checkout my projects.",
+    "Hello there👋 I'm Mori. I'm a software and hardware engineer. Checkout my projects.",
+  icons: {
+    icon: [
+      {
+        url: "/favicon-dark.ico",
+        media: "(prefers-color-scheme: dark)",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
