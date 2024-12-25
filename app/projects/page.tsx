890e944d2515e31e43f2377787bd6ef29ffadd8e
Mori Adrian Rosland <mori@live.no>
2024-12-25 15:44:04 +0100

    feat: simple project layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,52 @@
 import { FaArrowLeft } from "react-icons/fa";
-import NavButtonIcon from "@/components/ui/NavButtonIcon";
+import NavButtonIcon from "@/components/ui/buttons/NavButtonIcon";
+import Card from "@/components/ui/cards/Card";
 
-export default function Projects() {
+interface Project {
+  _id: string;
+  title: string;
+  description: string;
+  tools: string[];
+}
+
+export default async function Projects() {
+  const projects: Project[] = await fetch(
+    "http://localhost:3000/api/projects"
+  ).then((res) => res.json());
+
   return (
     <main>
-      <NavButtonIcon href="/" icon={FaArrowLeft} text="Back" />
+      {/* Parent container uses relative so children can be positioned absolutely */}
+      <div className="relative flex items-center justify-center py-4">
+        {/* Position the nav button absolutely on the left */}
+        <div className="absolute left-0 px-4">
+          <NavButtonIcon icon={FaArrowLeft} href="/" text="Back" />
+        </div>
+
+        {/* Heading remains centered in the flex container */}
+        <h1 className="text-4xl font-bold text-center">Projects</h1>
+      </div>
+      <div className="flex flex-wrap gap-6 items-center justify-center">
+        {projects.map((project) => (
+          <Card
+            key={project._id}
+            header={project.title}
+            description={project.description}
+            imageUrl="/arcane.jpeg"
+          />
+        ))}
+      </div>
     </main>
   );
+}
+
+{
+  /* <div className="flex my-4">
+<div className="flex">
+  <NavButtonIcon icon={FaArrowLeft} href="/" text="Back" />
+</div>
+<div className="flex grow justify-center">
+  <h1 className="text-4xl font-bold text-center">Projects</h1>
+</div>
+</div> */
 }
--- components/ui/buttons/NavButtonIcon.tsx
+++ components/ui/buttons/NavButtonIcon.tsx
@@ -15,7 +15,7 @@ export default function NavButtonIcon({
   return (
     <Link
       href={href}
-      className="inline-flex flex-row gap-2 justify-center items-center text-black dark:text-white px-4 py-2 rounded-md hover:text-blue-500 transition duration-200"
+      className="inline-flex flex-row gap-2 justify-center items-center text-black dark:text-white rounded-md hover:text-blue-500 transition duration-200"
     >
       <Icon size={14} />
       <p className="text-md">{text}</p>
