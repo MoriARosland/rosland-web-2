dc6382045b331945f2f6399691fdfc3a0a88d3ff
Mori Adrian Rosland <mori@live.no>
2024-12-27 16:37:20 +0100

    chore: remove unused button code

--- components/ui/buttons/AuthButtons.tsx
+++ components/ui/buttons/AuthButtons.tsx
@@ -1,21 +1,6 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
-import Link from "next/link";
-
-export function SignInButton() {
-  const { status } = useSession();
-
-  if (status === "loading") {
-    return <>...</>;
-  }
-
-  if (status === "authenticated") {
-    return <Link href="/nexus">Nexus</Link>;
-  }
-
-  return <button onClick={() => signIn()}>Sign In</button>;
-}
+import { signOut } from "next-auth/react";
 
 export function SignOutButton() {
   return (
