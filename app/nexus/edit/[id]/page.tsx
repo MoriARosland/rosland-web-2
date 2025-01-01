546c8c359609f5451515093e9d09adf94dc6b6b2
Mori Adrian Rosland <mori@live.no>
2025-01-01 11:39:54 +0100

    feat: edit projects

--- app/nexus/edit/[id]/page.tsx
+++ app/nexus/edit/[id]/page.tsx
@@ -1,3 +1,5 @@
+import EditProject from "@/components/ui/forms/EditProject";
+
 interface Props {
   params: {
     id: string;
@@ -5,5 +7,29 @@ interface Props {
 }
 
 export default async function EditProjectPage({ params }: Props) {
-  return <div>placeholder</div>;
+  const id = (await params).id;
+
+  const response = await fetch(
+    `http://localhost:3000/api/projects/getOne?query=${id}`
+  );
+
+  if (!response.ok) {
+    const { error } = await response.json();
+    console.log("Error: ", error);
+    return (
+      <main className="flex flex-col items-center px-20 py-5">
+        <h1 className="text-3xl font-bold">Project not found</h1>
+      </main>
+    );
+  }
+
+  const { project } = await response.json();
+  console.log("Project: ", project);
+
+  return (
+    <main className="flex flex-col items-center px-20 py-5">
+      <h1 className="text-3xl font-bold">Editing: {project.title}</h1>
+      <EditProject project={project} />
+    </main>
+  );
 }
