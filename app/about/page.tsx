a46dbf6d74d62b446f44caae40751ba15ddbecc1
Mori Adrian Rosland <mori@live.no>
2025-01-03 15:42:38 +0100

    feat: nav bar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,7 @@
-import NavButtonIcon from "@/components/ui/buttons/NavButtonIcon";
-import { FaArrowLeft } from "react-icons/fa";
-
 export default function About() {
   return (
     <main className="flex flex-col">
-      <div className="relative flex items-center justify-center py-4">
-        <div className="absolute left-0 px-4">
-          <NavButtonIcon icon={FaArrowLeft} href="/" text="Back" />
-        </div>
-        <h1 className="text-4xl font-bold text-center">About</h1>
-      </div>
+      <h1 className="text-4xl font-bold text-center py-10">About</h1>
     </main>
   );
 }
