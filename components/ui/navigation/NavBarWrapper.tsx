9d08b4e72bf412585eb1fce0d7b5b811bca61753
Mori Adrian Rosland <mori@live.no>
2025-01-03 15:45:30 +0100

    fix: navbar showing in nexus

--- components/ui/navigation/NavBarWrapper.tsx
+++ components/ui/navigation/NavBarWrapper.tsx
@@ -5,7 +5,7 @@ import NavBar from "./NavBar";
 
 export default function NavBarWrapper() {
   const pathname = usePathname();
-  const showNavbar = pathname !== "/";
+  const showNavbar = pathname !== "/" && !pathname.startsWith("/nexus");
 
   if (!showNavbar) return null;
   return <NavBar />;
