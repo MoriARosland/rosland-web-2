8f258a114767d6aab458cafc50d5c6f51dd1d294
Mori Adrian Rosland <mori@live.no>
2025-01-03 16:33:01 +0100

    feat: about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,62 @@
+import Image from "next/image";
+
 export default function About() {
   return (
-    <main className="flex flex-col">
-      <h1 className="text-4xl font-bold text-center py-10">About</h1>
+    <main className="flex flex-col px-20 py-10">
+      <div className="flex flex-col gap-y-4 max-w-[605px]">
+        <h1 className="text-4xl font-bold">About</h1>
+        <div>
+          <h2 className="text-xl font-bold">Name</h2>
+          <p className="text-md">Mori Adrian Rosland.</p>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">Age</h2>
+          <p className="text-md">Continuously increasing.</p>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">Where I&apos;m From</h2>
+          <p className="text-md">
+            A place with plenty of fish and oil. Which part? The place where
+            people call us &quot;Nordlending&quot;.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">Education</h2>
+          <p className="text-md">
+            I&apos;m studying for a master&apos;s in Electronic System Design
+            with a specialization in Digital and Embedded Systems. Time spent
+            programming, is time spent well 🤖.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">Bio</h2>
+          <p className="text-md">
+            My interests can be rather quickly summarized: competitive sports,
+            programming, and reading. With this, I am a happy human 😌.
+          </p>
+          <p>
+            I will never say no to a competition 😈 — sports or otherwise 🚀.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-10 py-10">
+        <Image
+          src="/17mai.jpeg"
+          alt="17mai"
+          width={300}
+          height={300}
+          className="object-cover rounded-lg shadow-md shadow-black/25 dark:shadow-white/25"
+          priority
+        />
+        <Image
+          src="/cycling.jpeg"
+          alt="cycling"
+          width={300}
+          height={300}
+          className="object-cover rounded-lg shadow-md shadow-black/25 dark:shadow-white/25"
+          priority
+        />
+      </div>
     </main>
   );
 }
