4e9f8b52f7db7f80db3e6ed34098d99b19693b21
Mori Adrian Rosland <mori@live.no>
2025-01-04 14:30:21 +0100

    feat(LinkButton): new tab on click

--- components/ui/buttons/LinkButton.tsx
+++ components/ui/buttons/LinkButton.tsx
@@ -3,12 +3,31 @@ import { IconType } from "react-icons";
 
 interface LinkButtonProps {
   icon: IconType;
-  url: string;
+  url?: string;
+  disabled?: boolean;
 }
 
-export default function LinkButton({ icon: Icon, url }: LinkButtonProps) {
+export default function LinkButton({
+  icon: Icon,
+  url,
+  disabled = false,
+}: LinkButtonProps) {
+  if (disabled) {
+    return (
+      <Icon
+        size={28}
+        className="text-gray-400 dark:text-gray-600 cursor-not-allowed"
+      />
+    );
+  }
+
   return (
-    <Link href={url} prefetch={false}>
+    <Link
+      href={url!}
+      target="_blank"
+      rel="noopener noreferrer"
+      prefetch={false}
+    >
       <Icon
         size={28}
         className="text-black dark:text-white hover:scale-110 hover:fill-sky-500 transition duration-200"
