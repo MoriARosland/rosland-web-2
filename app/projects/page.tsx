cdfe88fe1ef008444a852a8709033472c3aca535
Mori Adrian Rosland <mori@live.no>
2025-01-05 12:25:34 +0100

    perf: query database directly

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,43 +1,53 @@
 import Card from "@/components/ui/cards/Card";
+import clientPromise from "@/lib/mongodb";
 import { Project } from "@/lib/types/project";
-import { BASE_API_URL } from "@/lib/utils/constants";
 
-export const dynamic = "force-dynamic";
-
-export default async function Projects() {
-  let projects: Project[] = [];
-
+async function getProjectData() {
   try {
-    // Try production URL first
-    const res = await fetch(`${BASE_API_URL}/api/projects`);
-    if (!res.ok) {
-      // If production fails, try localhost
-      const localRes = await fetch(`http://localhost:3000/api/projects`);
-      projects = await localRes.json();
-    } else {
-      projects = await res.json();
-    }
+    const client = await clientPromise;
+    const db = client.db(process.env.DATABASE_NAME);
+
+    const response = await db.collection("projects").find({}).toArray();
+    const projects: Project[] = response.map((project) => ({
+      _id: project._id.toString(),
+      title: project.title as string,
+      abstract: project.abstract as string,
+      description: project.description as string,
+      tag: project.tag as string,
+      github: project.github as string,
+      techStack: project.techStack as string[],
+    }));
+
+    return projects;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    console.error("Error fetching projects:", error);
-    // Return empty array if both attempts fail
-    projects = [];
+    return [];
   }
+}
+
+export default async function ProjectsPage() {
+  // Data revalidation is triggered by the form components using server actions.
+  const projects: Project[] = await getProjectData();
 
   return (
     <main className="flex flex-col">
       <h1 className="text-4xl font-bold text-center py-10">Projects</h1>
 
-      <div className="flex flex-wrap gap-6 items-center justify-center">
-        {projects.map((project) => (
-          <Card
-            id={project._id}
-            key={project._id}
-            title={project.title}
-            abstract={project.abstract}
-            tag={project.tag}
-          />
-        ))}
-      </div>
+      {projects.length === 0 ? (
+        <p className="text-center text-xl">No projects found</p>
+      ) : (
+        <div className="flex flex-wrap gap-6 items-center justify-center">
+          {projects.map((project: Project) => (
+            <Card
+              id={project._id}
+              key={project._id}
+              title={project.title}
+              abstract={project.abstract}
+              tag={project.tag}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
