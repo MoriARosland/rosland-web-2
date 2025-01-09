a41c06ee12283f3d34f1d3d7455bd371d72f7493
Mori Adrian Rosland <mori@live.no>
2025-01-09 20:14:52 +0100

    style: update color and padding for nexus navbar

--- components/ui/navigation/NexusNavbar.tsx
+++ components/ui/navigation/NexusNavbar.tsx
@@ -3,7 +3,7 @@ import { SignOutButton } from "@/components/ui/buttons/AuthButtons";
 
 export default function NexusNavbar() {
   return (
-    <div className="flex justify-between items-center p-4 bg-zinc-900">
+    <div className="flex justify-between items-center py-4 bg-gray-100 dark:bg-zinc-900 px-14">
       <Link href="/nexus" className="font-semibold">
         Nexus
       </Link>
