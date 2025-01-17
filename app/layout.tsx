c55bb070a907f0e4f732064ed97e30323b25cbc1
Mori Adrian Rosland <mori@live.no>
2025-01-17 16:17:18 +0100

    feat: Insights and analytics (#15)
    * chore: remove tests
    
    * feat: Vercel analytics and speed insights
    
    * chore: remove vite.config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Titillium_Web } from "next/font/google";
 import "./globals.css";
 import NavBarWrapper from "@/components/ui/navigation/NavBarWrapper";
@@ -31,6 +33,8 @@ export default function RootLayout({
       <body className={`${titillium.className} antialiased`}>
         <NavBarWrapper />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
